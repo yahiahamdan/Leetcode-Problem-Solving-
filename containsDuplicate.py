102d132c698a03768a0fe2220da22fb9b08350c3
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-03 01:42:41 +0300

    solving containsDuplicate

---
+++ containsDuplicate.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+            dic={}
+            for i in range(len(nums)):
+               if(nums[i] in dic):
+                   return True
+               dic[nums[i]]=True
+            return False             
+                
