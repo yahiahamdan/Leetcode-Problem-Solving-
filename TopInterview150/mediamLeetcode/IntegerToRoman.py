9584e7637350ed206694f5d52392248a25458072
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-04 03:26:39 +0300

    solving integer to roman question in leetcode

---
+++ TopInterview150/mediamLeetcode/IntegerToRoman.py
@@ -0,0 +1,19 @@
+class Solution(object):
+      def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        symbols=[
+       ["I",1],["IV",4],["V",5],["IX",9],
+       ["X",10],["XL",40] ,["L",50],["XC",90],
+       ["C",100], ["CD",400],["D",500],
+       ["CM",900],["M",1000]
+          ]
+        res=""
+        for ch,val in reversed(symbols):
+           if  num // val :
+             count= num // val
+             res +=(count * ch)
+             num=num%val
+        return res
