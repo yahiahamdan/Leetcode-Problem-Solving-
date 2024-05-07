a1258e263dbae72cd088032e9c120194cda87ae2
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-07 03:46:33 +0300

    adding find the first occurance string index

---
+++ TopInterview150/easyLeetcode/find THeIndexOfFirstOccuranceString.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        index:0  -1  first occurance of string
+        ana 3ayz lw p2==len(needle) return p1-len(needle) 
+        """
+        return haystack.finc(needle)
+    
+         
