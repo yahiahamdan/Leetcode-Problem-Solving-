c0bcbed4245afcb866ceadc3951ceb49e3a1f797
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-07 18:50:32 +0300

    solving first ouccurance index string with another approach

--- TopInterview150/easyLeetcode/find THeIndexOfFirstOccuranceString.py
+++ TopInterview150/easyLeetcode/find THeIndexOfFirstOccuranceString.py
@@ -7,6 +7,16 @@ class Solution(object):
         index:0  -1  first occurance of string
         ana 3ayz lw p2==len(needle) return p1-len(needle) 
         """
-        return haystack.finc(needle)
+        return haystack.find(needle)
+def strStr2(self,haystack,needle):
+        for start in range(len(haystack)-len(needle)+1):
+            match=True
+            for j in range(len(needle)):
+             if(haystack[start+j]!=needle[j]):
+                match=False
+                break
+            if(match):
+                return start            
+        return -1
     
          
