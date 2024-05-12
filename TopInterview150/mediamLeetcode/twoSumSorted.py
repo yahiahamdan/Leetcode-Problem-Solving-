b65cda2450eb774b0fc2bfaeee4f727e3167f535
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-12 14:35:23 +0300

    solving twosumsorted problem leetcode mediam

---
+++ TopInterview150/mediamLeetcode/twoSumSorted.py
@@ -0,0 +1,12 @@
+def twosumSorted(nums,target):
+    left=0
+    right=len(nums)-1
+    while(left<right):
+        sum=nums[left]+nums[right]
+        if sum==target:
+            return [left,right]
+        elif sum<target:
+            left+=1
+        else :
+            right-=1
+print(twosumSorted([2,7,11,15],9))
