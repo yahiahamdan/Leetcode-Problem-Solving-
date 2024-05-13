f1b4259a188f237b227023f3ac19b1c2ba125a01
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-13 16:00:02 +0300

    solving the smalledelementrows using sets

--- TopInterview150/easyLeetcode/2sum.py
+++ TopInterview150/easyLeetcode/2sum.py
@@ -1,7 +1,19 @@
+#first approach to solve this problem using one loop for hash one loop for checking 
 def twosum(nums,target):
-    hasdict={nums[i]:i for i in range(len(nums))}
+    hasdict= {nums[i]:i for i in range(len(nums)) }
     for i in range(len(nums)):
-        diff=target-nums[i]
-        if(diff  in hasdict):
-          return [i,hasdict[diff]]
-print(twosum([3,2,4],6))
+           diff=target-nums[i]
+           if(diff in hasdict and hasdict[diff]!=i):
+            return [i,hasdict[diff]]
+    return []
+#second approach to solve this problem 
+#lets do anothedr approach  using the same hashmap 
+def twosum2(nums,target):
+    hasdit={}
+    for i in range(len(nums)):
+      diff=target-nums[i]
+      if diff not in hasdit:
+          hasdit[nums[i]]=i
+      else:
+          return [i,hasdit[diff]]
+    return []
