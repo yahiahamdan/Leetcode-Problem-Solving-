5760a0a46c43730ea09eecf4bdd9dd19de875a6b
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-14 10:25:48 +0300

    solving smalledElementInRows

---
+++ TopInterview150/mediamLeetcode/smallestELementRows.py
@@ -0,0 +1,15 @@
+"""
+given matrix mat  where every row is sorted in increasing order 
+return the smallest common element in all rows
+mat= [[1,2,3,4,5],[2,4,5,8,10],[3,5,7,9,11],[1,3,5,7,9]]
+"""
+def finc_common_elements(matrix):
+
+ common_elements=set(matrix[0])
+ for row in matrix[1:]:
+   common_elements &=set(row)
+   sortedList=sorted(common_elements)
+ return (sortedList[0])
+
+print(finc_common_elements([[1,2,3,4,5],[2,3,5,8,10],[3,5,7,9,11],[1,3,5,7,9]]))        
+
