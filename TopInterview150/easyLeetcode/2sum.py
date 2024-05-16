4c42447e742ed891b5866bba91ec98ae19579a52
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-16 18:47:56 +0300

    updating 2sum problem

--- TopInterview150/easyLeetcode/2sum.py
+++ TopInterview150/easyLeetcode/2sum.py
@@ -17,3 +17,24 @@ def twosum2(nums,target):
       else:
           return [i,hasdit[diff]]
     return []
+def twosum3(nums,target):
+    listT=[]
+    res=[]
+    for i in range(len(nums)-1):
+        
+        for j in range(i+1,len(nums)):
+            if nums[i]+nums[j]==target:
+                listT.append([i,j])
+    res.append(listT)
+    return res
+print (twosum3([1,2,3,4,5,6,7,8,9],9))
+def twosum4(nums, target):
+    hasdict = {}
+    for i in range(len(nums)):
+        hasdict[nums[i]] = i
+    for i in range(len(nums)):
+        diff = target - nums[i]
+        if diff in hasdict:
+            return [i, hasdict[diff]]
+
+print(twosum4([1, 2, 3, 4, 5, 6, 7, 8, 9], 9))
