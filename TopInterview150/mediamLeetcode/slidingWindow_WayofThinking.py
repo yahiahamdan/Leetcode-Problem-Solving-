80bbfff74ac9e2e315f623f438dacd39d4ea449d
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-17 14:38:43 +0300

    adding techniquie of sliding window

---
+++ TopInterview150/mediamLeetcode/slidingWindow_WayofThinking.py
@@ -0,0 +1,39 @@
+""""
+now lets think about subarray problems (max,min subarray length or find the 
+or substringss accorginto a given set of conditions
+find subarray ,
+ find the longest substring with unique 
+find the longest substring with unique characters or oslving problems 
+fixed size window 
+2 for loop we tshoof every possible subarray 
+y2ma efficent approach any a3ml array of also integet size  
+doing n2 time compliexity 
+optimize this opperation ahh mmkn a3mlha fe k  size subarray
+update the previous sum as shown 
+k subarray window 0 ,l- 
+the result like 
+current sum =9 
+max sum=9  kol shwya hincrease current sum 
+left pointer right pointer 
+the sum of each sum array 
+feha nw3eeen     ffixed size sliding window 
+varaible size sliding window 
+        fixed size sliding window 
+) 
+fixed (
+required say ke cimmpute the result include the k elements of the data structure 
+for loop to slide the window by oen
+
+)
+varaible (
+1_ lw condition btrue inc right pointer inc window
+lw b flase inc left pointer  shrink the window 
+)
+idendity the problem (
+require finding the  
+subarray ,substring, o(n2) 
+to find the maxi sum of 
+)
+
+
+"""
