e1fe87bd499d9872160e6f247f7468a002097b11
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-20 14:55:48 +0300

    custom sort problems by digit then by uppercase then smaller

---
+++ mediamLeetcode/customSort.py
@@ -0,0 +1,20 @@
+"""
+i have a string and i want to do a custom sort  for example i want to make no then upper then lower 
+output A1b2xe3   result = 123Abex
+"""
+def customSort(s):
+    s=sorted(s,key=lambda x:(x.isdigit(),x.isupper(),x))
+    return ''.join(s)
+print(customSort("A1b2xe3")) 
+
+def customsort2(s):
+    for i in range(len(s)):
+        if s[i].isdigit():
+          return (0,s[i])
+        elif s[i].isupper():
+            return (1,s[i])
+        else:
+            return (2,s[i])
+
+s="A1b2xe3"
+print(''.join(sorted(s,key=customsort2)))
