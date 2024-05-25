275c52e6150648e7c906830c4c9389ecf59d12bc
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-26 00:23:59 +0300

    solvign longest palindrome using brute forces solution

---
+++ mediamLeetcode/longestpalindrome.py
@@ -0,0 +1,15 @@
+def isPlaindrome(string):
+    news=string[::-1]
+    return news==string
+def longestpalindrom(string):
+    maxlength=0
+    for i in range(len(string)):
+        for j in range(i+1,len(string)+1):
+            newSubstring=string[i:j]
+            #print(newSubstring)
+            if isPlaindrome(newSubstring):
+                maxlength=max(maxlength,len(newSubstring))
+    return maxlength
+    #return maxlength if maxlength !=float('-inf') else 0 
+
+print(longestpalindrom("babad"))
