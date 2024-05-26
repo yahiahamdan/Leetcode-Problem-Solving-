d57a32ec606a4c8865062dc331dead8fa44a27da
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-26 19:52:19 +0300

    smallest element in rows

--- mediamLeetcode/smallestELementRows.py
+++ mediamLeetcode/smallestELementRows.py
@@ -3,19 +3,16 @@ given matrix mat  where every row is sorted in increasing order
 return the smallest common element in all rows
 mat= [[1,2,3,4,5],[2,4,5,8,10],[3,5,7,9,11],[1,3,5,7,9]]
 """
-def finc_common_elements(matrix):
-
- common_elements=set(matrix[0])
- for row in matrix[1:]:
-   common_elements &=set(row)
-   sortedList=sorted(common_elements)
- return (sortedList[0])
-
-print(finc_common_elements([[1,2,3,4,5],[2,3,5,8,10],[3,5,7,9,11],[1,3,5,7,9]]))        
-
 keydict={'x':1,'b':5,'a':3}
 sortedKeys=(sorted(keydict))
-sortedValues=sorted(keydict,key=keydict.get)
+
+reversedArray= reversed(list(keydict.items()))
 print(sortedKeys)
 print(" ==== ")
-print(sortedValues)
+
+def smallestElementRows(matrix):
+    commenElements=set(matrix[0])
+    for row in range(1,len(matrix)):
+        commenElements&=set(matrix[row])
+    return list(commenElements)[-1]
+print(smallestElementRows([[1,2,3,4,5],[2,4,5,8,10],[2,5,4,9,11],[2,3,5,7,4]]))
