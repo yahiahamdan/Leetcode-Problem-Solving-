93100b0e9e9f6899a20605368eb25838bd111805
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-27 22:16:58 +0300

    solving sort studentsbykthscore

--- mediamLeetcode/sortStudentsByKthScore.py
+++ mediamLeetcode/sortStudentsByKthScore.py
@@ -2,6 +2,7 @@
 ana endy fucntion matrix i[student,examj]  
 k is the index of the exam   
 a sort the row 
+3ayz a sort student by thier kth order 
 """
 class Solution(object):
     def sortTheStudents(self, score, k):
