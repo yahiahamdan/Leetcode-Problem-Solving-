cea858d853053bfc8a7da2e9974fcbcb2b0f4e34
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-28 09:21:40 +0300

    remove duplicate from sorted array

---
+++ mediamLeetcode/removeDuplicateSortedArray2.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        k=1
+        for i in range(1,len(nums)):
+            if nums[i-1] !=nums[i]:
+                nums[k]=nums[i]
+                k+=1
+        return k            
