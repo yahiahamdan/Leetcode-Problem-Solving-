93020b3e4835fd3548ee794d292f05eb6d714b87
yahia hamdan <hamdanyahia2@gmail.com>
2024-05-30 09:55:45 +0300

    adding simplified path problem

--- mediamLeetcode/simplifyPath.py
+++ mediamLeetcode/simplifyPath.py
@@ -1,11 +1,12 @@
 #solv ed in this leetcode but needed upgradation
-def simplifyPath(self, path):
+def simplifyPath(path):
         """
         :type path: str
         :rtype: str
         """
         stack=[]
         words=path.split('/')
+        print(words)
         for ch in words : 
             if ch == '.' or ch== '':
               continue
@@ -15,5 +16,22 @@ def simplifyPath(self, path):
             else:
                 stack.append(ch)
         return "/"+('/'.join(stack))
-print(simplifyPath("/a/"))
+print(simplifyPath("/a//efwe/"))
+
+def simplifyPath(path2):
+    stack=[]
+    cur=""
+    for c in path2:
+        if c=="/":
+            if cur=="..":
+                if stack:
+                    stack.pop()
+            elif cur!="" and cur!=".":
+                stack.append(cur)
+            cur=""
+        else:
+           cur+=c
+                
+    return "/"+"/".join(stack) 
+      
 
