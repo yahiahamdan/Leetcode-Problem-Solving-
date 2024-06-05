f961f0b163220739ab53bdfd49f50b7376ff39a7
yahia hamdan <hamdanyahia2@gmail.com>
2024-06-05 22:08:50 +0300

    solving find common chars

---
+++ mediam/FindCommonChars.py
@@ -0,0 +1,21 @@
+from collections import Counter
+def findCommonChars(words):
+    firstCounter=Counter(words[0])
+    for word in words:
+        counterWord=Counter(word)
+        for key in firstCounter:
+           firstCounter[key]= min(firstCounter[key],counterWord[key])
+    return firstCounter
+
+print(findCommonChars(['labels','lab','labbey']))
+
+def findsinglechars(word):
+    newlist=[]
+    freqchar=Counter(word)
+    newlist= [key for key , value in freqchar.items() if value==1]
+    return newlist
+print(findsinglechars("aaabbbcde"))
+
+
+    
+
