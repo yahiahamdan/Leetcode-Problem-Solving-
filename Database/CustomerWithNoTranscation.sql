444701f315fa9be8437e5a6c1a4bdb1131d73eab
yahia hamdan <hamdanyahia2@gmail.com>
2024-06-06 20:42:11 +0300

    customer with no transcation problem solved

---
+++ Database/CustomerWithNoTranscation.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+
+select v.customer_id , count(v.visit_id) as count_no_trans  from Visits v left join Transactions t on  t.visit_id =v.visit_id
+where t.transaction_id is null group by customer_id  
