8590f71814747f665b54da989c3e7e9044a9f7d8
yahia hamdan <hamdanyahia2@gmail.com>
2024-06-08 22:54:07 +0300

    solving fizz buzz problem

---
+++ hackerRank/fizzBuzz.py
@@ -0,0 +1,9 @@
+def fizzbuzz(i):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0 and i % 5 != 0:
+        print("Fizz")
+    elif i % 3 != 0 and i % 5 == 0:
+        print ("Buzz")
+    else:
+        print(i)
