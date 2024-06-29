1274a31d7e803d13aac7e53aed1e712988414322
yahia hamdan <hamdanyahia2@gmail.com>
2024-06-29 21:23:06 +0300

    solving uniqueEmail Address problem

---
+++ easy/uniqueEmailAddress.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        """
+        :type emails: List[str]
+        :rtype: int
+        """
+        res=set()
+        for email in emails:
+            local,domain=email.split('@')
+            tmp=""
+            for c in local:
+                if c ==".":continue
+                elif c=="+":break
+                else: tmp+=c
+            res.add(tmp+'@'+domain)
+        return len(res)
+       
